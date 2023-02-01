ded9f98576eaef326e6ec9b2a00fef217e3223a9
Nosferatu <davonb113@gmail.com>
2023-02-01 12:28:52 -0600

    server

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const storeItems = ([
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, '/client/build'));
   });
 }
 
